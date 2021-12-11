a10bc59c69b44d46dbb43fe705c9aca886d6d672
gilgamesh7 <felidaesrule@yahoo.com>
2021-12-11 18:22:23 +1300

    Refactoring and raises exception test

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -1,11 +1,72 @@
+from dataclasses import dataclass, field
 
-class Checkout():
+import logging
+
+@dataclass
+class Checkout:
+    prices: dict[str,float] = field(default_factory=dict)
+    discounts: dict[str,float] = field(default_factory=dict)
+    items: dict[str,float] = field(default_factory=dict)
+    # total: float = field(default_factory=float)
+
+    @dataclass
+    class Discount:
+        number_of_items: int 
+        price: float
+
+
+    logging.basicConfig(level=logging.INFO)
+    logger=logging.getLogger()
+
+
     def add_item_price(self, item_name: str, price: float)-> None:
-        pass
+        self.prices[item_name] = price
+        self.logger.info(f"Prices : {self.prices}")
 
     def add_item(self, item_name: str)-> None:
-        pass
+        # self.total += self.prices[item_name]
+        # if item_name in self.items:
+        #     self.items[item_name] += 1
+        # else:
+        #     self.items[item_name] = 1
+        if item_name not in  self.prices :
+            raise Exception(f"f{item_name} does not have a price")
+            
+        self.items[item_name] = 1 if item_name not in self.items else self.items[item_name]+1
 
-    def calculate_current_total(self)-> float:
-        return 20.5
+        self.logger.info(f"Items : {self.items}")
+
+    def add_discount(self, item_name:str , num_of_items_to_qualify: int, new_price: float)-> None:
+        discount = self.Discount(num_of_items_to_qualify, new_price)
+        self.discounts[item_name] = discount
         
+    def calculate_current_total(self)-> float:                
+        # self.logger.info(f"Total : {self.total}")
+        total = 0
+        for item_name, count in self.items.items():
+            total += self.calculate_item_total(item_name, count)
+        return total
+
+    def calculate_item_total(self, item_name:str, count:int)-> float:
+        total=0
+        if item_name in self.discounts:
+            discount = self.discounts[item_name]
+            if count >= discount.number_of_items:
+                total += self.calculate_item_discounted_total(item_name, count, discount)
+            else:
+                total += self.prices[item_name] * count
+        else:
+            total += self.prices[item_name] * count
+
+        return total
+
+    def calculate_item_discounted_total(self, item_name: str, count: int, discount: Discount)-> float:
+        total = 0
+
+        no_of_discounts = count/discount.number_of_items
+        total += no_of_discounts * discount.price
+        remaining = count % discount.number_of_items
+        total += remaining * self.prices[item_name]
+
+        return total
+
--- checkout/test_checkout.py
+++ checkout/test_checkout.py
@@ -24,11 +24,12 @@ from checkout import Checkout
 #     checkout = Checkout()
 #     checkout.add_item("Item1")
 
-@pytest.fixture()
-def checkout()-> Checkout:
-    checkout = Checkout()
+# Refactored - As additem price is repeated
+# @pytest.fixture()
+# def checkout()-> Checkout:
+#     checkout = Checkout()
 
-    return checkout
+#     return checkout
 
 # REFACTORED - as these 2 areimplemented in test_CanCalculateCurrentTotal
 # # Can add item price
@@ -39,13 +40,66 @@ def checkout()-> Checkout:
 # def test_CanAddItem(checkout):
 #     checkout.add_item("Item1")
 
+# REFACTORED - add item is now part of fixture
+# # can calculate current total
+# def test_CanCalculateCurrentTotal(checkout):
+#     checkout.add_item_price("Item1",20.5)
+#     checkout.add_item("Item1")
+
+#     assert checkout.calculate_current_total() == 20.5
+
+# # can add multiple items and getcurrent total
+# def test_CanAddMultipleItemsAndCalculateCorrectTotal(checkout):
+#     checkout.add_item_price("Item1",20.5)
+#     checkout.add_item_price("Item2",30.5)
+#     checkout.add_item("Item1")
+#     checkout.add_item("Item2")
+
+#     assert checkout.calculate_current_total() == 51
+# # can calculate current total
+# def test_CanCalculateCurrentTotal(checkout):
+#     checkout.add_item_price("Item1",20.5)
+#     checkout.add_item("Item1")
+
+#     assert checkout.calculate_current_total() == 20.5
+
+@pytest.fixture()
+def checkout()-> Checkout:
+    checkout = Checkout()
+    checkout.add_item_price("Item1",20.5)
+    checkout.add_item_price("Item2",30.5)
+
+    return checkout
+
 # can calculate current total
 def test_CanCalculateCurrentTotal(checkout):
-    checkout.add_item_price("Item1",20.5)
     checkout.add_item("Item1")
 
     assert checkout.calculate_current_total() == 20.5
 
+# can add multiple items and getcurrent total
+def test_CanAddMultipleItemsAndCalculateCorrectTotal(checkout):
+    checkout.add_item("Item1")
+    checkout.add_item("Item2")
 
+    assert checkout.calculate_current_total() == 51
 
+# REFACTORING - part of can apply discounts
+# can add discount rules
+# @pytest.mark.skip
+# def test_CanAddDiscountRules(checkout):
+#     checkout.add_discount("Item1", 3, 2.5)
+
+# @pytest.mark.skip
+def test_CanApplyDiscounts(checkout):
+    checkout.add_discount("Item1", 3, 10.4)
+    checkout.add_item("Item1")
+    checkout.add_item("Item1")
+    checkout.add_item("Item1")
+
+    assert checkout.calculate_current_total() == 10.4
+
+def test_ThrowExceptionForItemWithNoPrice(checkout):
+    with pytest.raises(Exception):
+        checkout.add_item("Item3")
 
